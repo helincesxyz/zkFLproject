a34b87ea1660eb5bc444daede945e47966b20a09
helincesxyz <helincesxyz>
2024-07-13 23:28:59 +0200

    risczero

---
+++ zkfl_risczero/target/debug/deps/zkfl_risczero-ad4120f7d7001369.d
@@ -0,0 +1,5 @@
+/Users/cookie/Desktop/zkFLproject/zkfl_risczero/target/debug/deps/libzkfl_risczero-ad4120f7d7001369.rmeta: src/main.rs
+
+/Users/cookie/Desktop/zkFLproject/zkfl_risczero/target/debug/deps/zkfl_risczero-ad4120f7d7001369.d: src/main.rs
+
+src/main.rs:
---
+++ zkfl_risczero/target/debug/deps/zkfl_risczero-cef03c1e5d834d8d.d
@@ -0,0 +1,5 @@
+/Users/cookie/Desktop/zkFLproject/zkfl_risczero/target/debug/deps/libzkfl_risczero-cef03c1e5d834d8d.rmeta: src/main.rs
+
+/Users/cookie/Desktop/zkFLproject/zkfl_risczero/target/debug/deps/zkfl_risczero-cef03c1e5d834d8d.d: src/main.rs
+
+src/main.rs:
