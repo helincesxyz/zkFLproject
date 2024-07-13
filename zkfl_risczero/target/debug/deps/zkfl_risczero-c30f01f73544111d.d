437b49d2283624f565f2db220cbf28a4195a0f4a
helincesxyz <helincesxyz>
2024-07-13 21:14:57 +0200

    RiscZero component

---
+++ zkfl_risczero/target/debug/deps/zkfl_risczero-c30f01f73544111d.d
@@ -0,0 +1,5 @@
+/Users/cookie/Desktop/zkFLproject/zkfl_risczero/target/debug/deps/zkfl_risczero-c30f01f73544111d: src/main.rs
+
+/Users/cookie/Desktop/zkFLproject/zkfl_risczero/target/debug/deps/zkfl_risczero-c30f01f73544111d.d: src/main.rs
+
+src/main.rs:
---
+++ zkfl_risczero/target/debug/zkfl_risczero.d
@@ -0,0 +1,1 @@
+/Users/cookie/Desktop/zkFLproject/zkfl_risczero/target/debug/zkfl_risczero: /Users/cookie/Desktop/zkFLproject/zkfl_risczero/src/main.rs
